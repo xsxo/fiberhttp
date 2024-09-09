0c1411debfa47374de4730a6e34834d3d34c667a
osmz <ssaasskkee44@gmail.com>
2024-09-09 19:42:51 +0300

    v1.1 release version

--- build/lib/fiberhttp/__init__.py
+++
@@ -1,6 +0,0 @@
-from .client_file import client
-from .methods import get, post, put, delete
-# from os import path
-# from sys import path as syspath
-
-# syspath.append(path.abspath(path.dirname(__file__)))
